742bd0e01de86f11ba732b003394231f934609dd
Vildar Khisyametdinov <vildar82@gmail.com>
2015-11-13 17:29:01 +0300

    switcher DB

---
+++ AcadLib/WorkingDatabaseSwitcher.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.AutoCAD.DatabaseServices;
+
+namespace AcadLib
+{
+   /// <summary>
+   /// This class switches the WorkingDatabase. It was created for using in the 
+   /// tests.
+   /// </summary>
+   public sealed class WorkingDatabaseSwitcher : IDisposable
+   {
+      Database oldDb = null;
+      /// <summary>
+      /// Constructor.
+      /// </summary>
+      /// <param name="db">Target database.</param>
+      public WorkingDatabaseSwitcher(Database db)
+      {
+         oldDb = HostApplicationServices.WorkingDatabase;
+         HostApplicationServices. WorkingDatabase = db;
+      }
+
+      public void Dispose()
+      {
+         HostApplicationServices.WorkingDatabase = oldDb;
+      }
+   }
+}
