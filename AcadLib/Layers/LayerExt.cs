74ac415057c3a2a0dc38271bcd4b71e6bfa1bd3f
Vildar Khisyametdinov <vildar82@gmail.com>
2015-11-25 14:21:39 +0300

    CheckLayerState

---
+++ AcadLib/Layers/LayerExt.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.AutoCAD.DatabaseServices;
+
+namespace AcadLib.Layers
+{
+   public class LayerExt
+   {
+      /// <summary>
+      /// Получение слоя.
+      /// Если его нет в базе, то создается.      
+      /// </summary>
+      /// <param name="layer"></param>
+      /// <returns></returns>
+      public static ObjectId GetLayerOrCreateNew(LayerInfo layerInfo)
+      {
+         Database db = HostApplicationServices.WorkingDatabase;
+         // Если уже был создан слой, то возвращаем его. Опасно, т.к. перед повторным запуском команды покраски, могут удалить/переименовать слой марок.         
+         using (var lt = db.LayerTableId.Open(OpenMode.ForRead) as LayerTable)
+         {
+            if (lt.Has(layerInfo.Name))
+            {
+               return lt[layerInfo.Name];
+            }
+            // Если слоя нет, то он создается.            
+            var newLayer = new LayerTableRecord();
+            newLayer.Name = layerInfo.Name;
+            newLayer.Color = layerInfo.Color;
+            newLayer.IsFrozen = layerInfo.IsFrozen;
+            newLayer.IsLocked = layerInfo.IsLocked;
+            newLayer.IsOff = layerInfo.IsOff;
+            newLayer.IsPlottable = layerInfo.IsPlotable;
+            if (!layerInfo.LinetypeObjectId.IsNull)
+               newLayer.LinetypeObjectId = layerInfo.LinetypeObjectId;
+            lt.UpgradeOpen();
+            lt.Add(newLayer);
+         }
+         return ObjectId.Null;
+      }
+
+      /// <summary>
+      /// Проверка блокировки слоя IsOff IsLocked IsFrozen.
+      /// Если заблокировано - то разблокируется.
+      /// </summary>
+      /// <param name="layers">Список слоев для проверкм в текущей рабочей базе</param>
+      public static void CheckLayerState(string[] layers)
+      {
+         Database db = HostApplicationServices.WorkingDatabase;
+         using (var lt = db.LayerTableId.Open(OpenMode.ForRead) as LayerTable)
+         {
+            foreach (var layer in layers)
+            {
+               if (lt.Has(layer))
+               {
+                  using (var lay = lt[layer].Open(OpenMode.ForRead) as LayerTableRecord)
+                  {
+                     if (lay.IsLocked && lay.IsOff && lay.IsFrozen)
+                     {
+                        lay.UpgradeOpen();
+                        if (lay.IsOff)
+                        {
+                           lay.IsOff = false;
+                        }
+                        if (lay.IsLocked)
+                        {
+                           lay.IsLocked = false;
+                        }
+                        if (lay.IsFrozen)
+                        {
+                           lay.IsFrozen = false;
+                        }
+                     }
+                  }
+               }
+            }
+         }
+      }
+
+      public static void CheckLayerState(string layer)
+      {
+         string[] layers = new string[1];
+         layers[0] = layer;
+         CheckLayerState(layers);
+      }
+   }
+}
---
+++ AcadLib/Layers/LayerInfo.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.AutoCAD.Colors;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.GraphicsInterface;
+
+namespace AcadLib.Layers
+{
+   public class LayerInfo
+   {
+      private string _name;
+      private bool _isOff;
+      private bool _isFrozen;
+      private bool _isPlotable;
+      private bool _isLocked;
+      private Color _color;      
+      private ObjectId _linetypeObjectId;
+      private LineWeight _lineWeight;
+
+      public string Name
+      {
+         get { return _name; }
+         set { setName(value); }
+      }
+
+      public bool IsOff { get { return _isOff; } set { _isOff = value; } }
+      public bool IsFrozen { get { return _isFrozen; } set { _isFrozen = value; } }
+      public bool IsPlotable { get { return _isPlotable; } set { _isPlotable = value; } }
+      public bool IsLocked { get { return _isLocked; } set { _isLocked = value; } }
+      public Color Color { get { return _color; } set { _color = value; } }      
+      public LineWeight LineWeight { get { return _lineWeight; } set { _lineWeight = value; } }
+      public ObjectId LinetypeObjectId { get { return _linetypeObjectId; } set { _linetypeObjectId = value; } }
+
+      public LayerInfo (string name)
+      {
+         setName(name);
+         _color = Autodesk.AutoCAD.Colors.Color.FromColorIndex(ColorMethod.ByAci, 7);
+         _lineWeight = LineWeight.ByLineWeightDefault;
+         _isPlotable = true;
+      }
+
+      private void setName(string name)
+      {
+         if (name.IsValidDbSymbolName())         
+            _name = name;         
+         else         
+            throw new Exception(string.Format("Недопустимое имя слоя - {0}", name));         
+      }      
+   }
+}
