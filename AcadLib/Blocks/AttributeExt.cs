fb85e23151b61982cd0573e7c974b99e3458b21a
Vildar Khisyametdinov <vildar82@gmail.com>
2015-12-19 18:22:01 +0300

    AttributeExt

---
+++ AcadLib/Blocks/AttributeExt.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.AutoCAD.DatabaseServices;
+
+namespace AcadLib.Extensions
+{
+   /// <summary>
+   /// Расширенные методы AttributeReference
+   /// </summary>
+   public static class AttributeExt
+   {
+      public static bool Is (this AttributeReference attr, string tag)
+      {
+         return string.Equals(attr.TextString, tag, StringComparison.CurrentCultureIgnoreCase);
+      }
+   }
+}
