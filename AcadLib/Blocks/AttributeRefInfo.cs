5d2ceedd22d8c0efcd7ae936b90907e1bc44fff8
Vildar Khisyametdinov <vildar82@gmail.com>
2015-12-21 08:48:25 +0300

    AttributeRefInfo

--- AcadLib/Blocks/AttributeRefInfo.cs
+++ AcadLib/Blocks/AttributeRefInfo.cs
@@ -23,6 +23,34 @@ namespace AcadLib.Blocks
          _idAtrRef = attr.Id;
       }
 
+      /// <summary>
+      /// DBText - должен быть или AttributeDefinition или AttributeReference
+      /// иначе исключение ArgumentException
+      /// </summary>
+      /// <param name="attr"></param>
+      public AttributeRefInfo(DBText attr)
+      {
+         AttributeDefinition attdef = attr as AttributeDefinition;
+         if (attdef != null)
+         {
+            _tag = attdef.Tag;
+         }
+         else
+         {
+            AttributeReference attref = attr as AttributeReference;
+            if (attref != null)
+            {
+               _tag = attref.Tag;
+            }
+            else
+            {
+               throw new ArgumentException("requires an AttributeDefintion or AttributeReference");
+            }
+         }
+         _text = attr.TextString;
+         _idAtrRef = attr.Id;
+      }
+
       public string Tag { get { return _tag; } }
       public string Text { get { return _text; } }
       public ObjectId IdAtrRef { get { return _idAtrRef; } }      
