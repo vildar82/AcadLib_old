590a477369e191de34e5d224e1a56276b8f24f42
Vildar Khisyametdinov <vildar82@gmail.com>
2015-12-22 16:25:40 +0300

    22

--- AcadLib/AcadAddin.cs
+++ AcadLib/AcadAddin.cs
@@ -1,41 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using Autodesk.AutoCAD.ApplicationServices;
-using Autodesk.AutoCAD.Runtime;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Reflection;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Autodesk.AutoCAD.ApplicationServices;
+//using Autodesk.AutoCAD.Runtime;
 
-[assembly:ExtensionApplication(typeof(AcadLib.AcadAddin))]
-[assembly:CommandClass(typeof(AcadLib.AcadAddin))]
+//[assembly:ExtensionApplication(typeof(AcadLib.AcadAddin))]
+//[assembly:CommandClass(typeof(AcadLib.AcadAddin))]
 
-namespace AcadLib
-{
-   public class AcadAddin : IExtensionApplication
-   {
-      private static string about = "\nБиблиотека AcadLib версия: " + Assembly.GetExecutingAssembly().GetName().Version;                 
+//namespace AcadLib
+//{
+//   public class AcadAddin : IExtensionApplication
+//   {
+//      private static string about = "\nБиблиотека AcadLib версия: " + Assembly.GetExecutingAssembly().GetName().Version;                 
 
-      [CommandMethod("PIK", "About-AcadLib", CommandFlags.Modal)]
-      public void AboutCommand ()
-      {
-         aboutMsg();
-      }     
+//      [CommandMethod("PIK", "About-AcadLib", CommandFlags.Modal)]
+//      public void AboutCommand ()
+//      {
+//         aboutMsg();
+//      }     
 
-      public void Initialize()
-      {
-         aboutMsg();
-      }
+//      public void Initialize()
+//      {
+//         aboutMsg();
+//      }
 
-      public void Terminate()
-      {         
-      }
+//      public void Terminate()
+//      {         
+//      }
 
-      private static void aboutMsg()
-      {
-         Document doc = Application.DocumentManager.MdiActiveDocument;
-         if (doc == null) return;
-         doc.Editor.WriteMessage(about);
-      }
-   }
-}
+//      private static void aboutMsg()
+//      {
+//         Document doc = Application.DocumentManager.MdiActiveDocument;
+//         if (doc == null) return;
+//         doc.Editor.WriteMessage(about);
+//      }
+//   }
+//}
--- AcadLib/Properties/AssemblyInfo.cs
+++ AcadLib/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.351.20")]
-[assembly: AssemblyFileVersion("1.0.351.20")]
+[assembly: AssemblyVersion("1.0.356.22")]
+[assembly: AssemblyFileVersion("1.0.356.22")]
