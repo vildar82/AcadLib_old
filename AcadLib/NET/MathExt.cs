13a8c6fa3dd9c3525716567cb5190f6fa4dc8b75
Vildar Khisyametdinov <vildar82@gmail.com>
2016-01-19 08:23:14 +0300

    ToRadians

---
+++ AcadLib/NET/MathExt.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcadLib
+{
+   public static class MathExt
+   {
+      /// <summary>
+      /// Преобразование градусов в радианы (Math.PI / 180.0)*angleDegrees
+      /// </summary>
+      /// <param name="angleDegrees">Угол в градусах</param>
+      /// <returns>Угол в радианах</returns>
+      public static double ToRadians(this double angleDegrees)
+      {
+         return angleDegrees * (Math.PI / 180.0);
+      }
+   }
+}
