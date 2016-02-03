df1aee869648ea81e3b77f56264cbd0da9585c9d
Vildar Khisyametdinov <vildar82@gmail.com>
2016-02-03 19:34:03 +0300

    Commands  About

---
+++ AcadLib/Commands.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+
+[assembly: CommandClass(typeof(AcadLib.Commands))]
+
+namespace AcadLib
+{
+   public class Commands
+   {
+      [CommandMethod("PIK", "PIK-Acadlib-About", CommandFlags.Modal)]
+      public void About ()
+      {
+         Document doc = Application.DocumentManager.MdiActiveDocument;
+         if (doc == null)
+         {
+            return;
+         }
+         Editor ed = doc.Editor;
+         var acadLibVer = Assembly.GetExecutingAssembly().GetName().Version;
+         ed.WriteMessage($"\nБиблиотека AcadLib версии {acadLibVer}");
+      }
+   }
+}
