cb67d045c4538d536b35e94df07abd2ab7dad2bb
Vildar Khisyametdinov <vildar82@gmail.com>
2016-02-22 10:57:46 +0300

    DbObjectsCountInfo

--- AcadLib/Commands.cs
+++ AcadLib/Commands.cs
@@ -5,6 +5,7 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
 using Autodesk.AutoCAD.EditorInput;
 using Autodesk.AutoCAD.Runtime;
 
@@ -25,6 +26,40 @@ namespace AcadLib
          Editor ed = doc.Editor;
          var acadLibVer = Assembly.GetExecutingAssembly().GetName().Version;
          ed.WriteMessage($"\nБиблиотека AcadLib версии {acadLibVer}");
+      }
+
+      [CommandMethod("PIK", "DbObjectsCountInfo", CommandFlags.Modal)]
+      public void DbObjectsCountInfo()
+      {
+         Document doc = Application.DocumentManager.MdiActiveDocument;
+         if (doc == null) return;
+         Database db = doc.Database;
+         Editor ed = doc.Editor;
+
+         Dictionary<string, int> allTypes = new Dictionary<string, int>();
+
+         for (long i = db.BlockTableId.Handle.Value; i < db.Handseed.Value; i++)
+         {
+            ObjectId id;
+            if (db.TryGetObjectId(new Handle(i), out id))
+            {
+               if (allTypes.ContainsKey(id.ObjectClass.Name))
+               {
+                  allTypes[id.ObjectClass.Name]++;
+               }
+               else
+               {
+                  allTypes.Add(id.ObjectClass.Name, 1);
+               }
+            }
+         }
+
+         var sortedByCount = allTypes.OrderBy(i => i.Value);
+
+         foreach (var item in sortedByCount)
+         {
+            ed.WriteMessage($"\n{item.Key} - {item.Value}");
+         }
       }
    }
 }
