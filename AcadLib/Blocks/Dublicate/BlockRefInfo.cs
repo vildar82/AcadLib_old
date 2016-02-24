da2d4d637fba4d00c00d21b5b4a32242ff5e14ae
Vildar Khisyametdinov <vildar82@gmail.com>
2016-02-24 19:14:21 +0300

    CheckDublicateBlocks

---
+++ AcadLib/Blocks/Dublicate/BlockRefInfo.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Geometry;
+
+namespace AcadLib.Blocks
+{
+   /// <summary>
+   /// Данные о вхождении блока 
+   /// </summary>
+   public class BlockRefInfo : IEqualityComparer<BlockRefInfo>, IEquatable<BlockRefInfo>
+   {
+      public static Tolerance Tolerance { get; set; } = new Tolerance(0.02, 10);
+
+      public string Name { get; set; }
+      public ObjectId IdBlRef { get; set; }
+      public Point3d Position { get; set; }
+      public double Rotation { get; set; }
+
+      public BlockRefInfo(BlockReference blRef, string blName = null)
+      {
+         IdBlRef = blRef.Id;
+         Position = blRef.Position;
+         if (string.IsNullOrEmpty(blName))
+         {
+            blName = blRef.GetEffectiveName();
+         }
+         Name = blName;
+         Rotation = blRef.Rotation;        
+      }
+
+      public bool Equals(BlockRefInfo other)
+      {
+         return Name.Equals(other.Name) &&
+                Position.IsEqualTo(other.Position, Tolerance) &&
+                Math.Abs(Rotation - other.Rotation) < Tolerance.EqualPoint;
+      }
+
+      public override bool Equals(object obj)
+      {
+         if (obj is BlockRefInfo)
+         {
+            return Equals((BlockRefInfo)obj);
+         }
+         else
+         {
+            return false;
+         }
+      }      
+
+      public bool Equals(BlockRefInfo x, BlockRefInfo y)
+      {
+         return x.Equals(y);
+      }
+
+      public int GetHashCode(BlockRefInfo obj)
+      {
+         return obj.GetHashCode();
+      }
+
+      public override int GetHashCode()
+      {
+         int hCode = Name.GetHashCode();// ^ Position.GetHashCode() ^ Rotation.GetHashCode();
+         return hCode.GetHashCode();
+      }
+   }
+}
---
+++ AcadLib/Blocks/Dublicate/CheckDublicateBlocks.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AcadLib.Errors;
+using Autodesk.AutoCAD.DatabaseServices;
+
+namespace AcadLib.Blocks
+{
+   /// <summary>
+   /// Проверка наложения блоков в пространстве модели
+   /// </summary>
+   public class CheckDublicateBlocks
+   {
+      public CheckDublicateBlocks()
+      {
+
+      }
+
+      public void Check()
+      {
+         Database db = HostApplicationServices.WorkingDatabase;
+         using (var ms = SymbolUtilityServices.GetBlockModelSpaceId(db).Open( OpenMode.ForRead) as BlockTableRecord)
+         {
+            Inspector.Clear();
+            List<BlockRefInfo> blrefInfos = new List<BlockRefInfo>();
+            // Получение всех блоков
+            foreach (var idEnt in ms)
+            {
+               using (var blRef = idEnt.Open( OpenMode.ForRead, false, true)as BlockReference)
+               {
+                  if (blRef == null) continue;
+                  BlockRefInfo blRefInfo = new BlockRefInfo(blRef);
+                  blrefInfos.Add(blRefInfo);
+               }
+            }
+            // Выбор будликатов
+            var dublicBlRefInfos = blrefInfos.GroupBy(g => g).Where(b => b.Count() > 1);//.SelectMany(s=>s);
+
+            foreach (var dublBlRefInfo in dublicBlRefInfos)
+            {
+               Inspector.AddError($"Дублирование блоков '{dublBlRefInfo.Key.Name}' - {dublBlRefInfo.Count()} шт. в точке {dublBlRefInfo.Key.Position.ToString()}",
+                  dublBlRefInfo.Key.IdBlRef, System.Drawing.SystemIcons.Error);
+            }
+
+            if (Inspector.HasErrors)
+            {
+               if (Inspector.ShowDialog() != System.Windows.Forms.DialogResult.OK)
+               {
+                  throw new Exception("Отменено пользователем.");
+               }
+            }
+         }
+      }
+   }   
+}
