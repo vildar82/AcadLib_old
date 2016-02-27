c3a46955c970728e5b77950b0a25ce3a4abbfd56
Vildar Khisyametdinov <vildar82@gmail.com>
2016-02-27 16:16:33 +0300

    LoadService

---
+++ AcadLib/PIK/LoadService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+namespace AcadLib
+{
+   /// <summary>
+   /// Загрузка вспомогательных сборок
+   /// </summary>
+   public static class LoadService
+   {
+      /// <summary>
+      /// Загрузка сборки SpecBlocks.dll - для создания спецификация блоков, в соответствии с настройками.
+      /// </summary>
+      public static void LoadSpecBlocks()
+      {         
+         // Загрузка сборки SpecBlocks
+         var dllSpecBlocks = Path.Combine(AutoCAD_PIK_Manager.Settings.PikSettings.LocalSettingsFolder, @"Script\NET\packages\SpecBlocks.dll");
+         if (File.Exists(dllSpecBlocks))
+         {
+            Assembly.LoadFrom(dllSpecBlocks);
+         }
+         else
+         {
+            throw new Exception($"Не найден файл {dllSpecBlocks}.");
+         }
+      }
+   }
+}
--- AcadLib/Properties/AssemblyInfo.cs
+++ AcadLib/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.57.136")]
-[assembly: AssemblyFileVersion("1.0.57.136")]
+[assembly: AssemblyVersion("1.0.58.137")]
+[assembly: AssemblyFileVersion("1.0.58.137")]
