1e603b2660e44d3d828202cc73726750887fbd0e
Vildar Khisyametdinov <vildar82@gmail.com>
2016-02-29 11:14:51 +0300

    GetDublicateBlocks if (blRef == null || !blRef.Visible) continue;

--- AcadLib/Properties/AssemblyInfo.cs
+++ AcadLib/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.60.140")]
-[assembly: AssemblyFileVersion("1.0.60.140")]
+[assembly: AssemblyVersion("1.0.60.141")]
+[assembly: AssemblyFileVersion("1.0.60.141")]
