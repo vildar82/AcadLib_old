512a94514ab4fdc55389f88d16ac784b59c18507
Vildar Khisyametdinov <vildar82@gmail.com>
2016-03-14 17:30:27 +0300

    Глюк в дин блоках при проверке дубликатов. Добавил транзакцию.

--- AcadLib/Model/Blocks/Dublicate/CheckDublicateBlocks.cs
+++ AcadLib/Model/Blocks/Dublicate/CheckDublicateBlocks.cs
@@ -11,159 +11,159 @@ using Autodesk.AutoCAD.Geometry;
 
 namespace AcadLib.Blocks.Dublicate
 {
-   /// <summary>
-   /// Проверка наложения блоков в пространстве модели
-   /// </summary>
-   public static class CheckDublicateBlocks
-   {
-      public static Tolerance Tolerance { get; set; } = new Tolerance(0.02, 10);
-      public static int DEPTH = 5;
-      private static int curDepth;
-      private static HashSet<ObjectId> attemptedblocks;
-      private static List<BlockRefDublicateInfo> AllDublicBlRefInfos;
-      private static Dictionary<string, Dictionary<PointTree, List<BlockRefDublicateInfo>>> dictBlRefInfos;
+    /// <summary>
+    /// Проверка наложения блоков в пространстве модели
+    /// </summary>
+    public static class CheckDublicateBlocks
+    {
+        public static Tolerance Tolerance { get; set; } = new Tolerance(0.02, 10);
+        public static int DEPTH = 5;
+        private static int curDepth;
+        private static HashSet<ObjectId> attemptedblocks;
+        private static List<BlockRefDublicateInfo> AllDublicBlRefInfos;
+        private static Dictionary<string, Dictionary<PointTree, List<BlockRefDublicateInfo>>> dictBlRefInfos;
 
-      public static void Check()
-      {
-         curDepth = 0;
-         Database db = HostApplicationServices.WorkingDatabase;
-         Inspector.Clear();
-         attemptedblocks = new HashSet<ObjectId>();
-         AllDublicBlRefInfos = new List<BlockRefDublicateInfo>();
-         dictBlRefInfos = new Dictionary<string, Dictionary<PointTree, List<BlockRefDublicateInfo>>>();
-         try
-         {
-            GetDublicateBlocks(SymbolUtilityServices.GetBlockModelSpaceId(db), Matrix3d.Identity, 0);
-
-            // дублирующиеся блоки
-            AllDublicBlRefInfos = dictBlRefInfos.SelectMany(s => s.Value.Values).Where(w => w.Count > 1)                                    
-                                    .SelectMany(s=>s.GroupBy(g=>g).Where(w=>w.Skip(1).Any()))
-                                    .Select(s =>
-                                       {
-                                          var bi = s.First();
-                                          bi.CountDublic = s.Count();
-                                          bi.Dublicates = s.Skip(1).ToList();
-                                          return bi;
-                                       }).ToList();
-
-            // Добавление дубликатов в результирующий список
-            //AddTransformedToModelDublic(dublicBlRefInfos);
-         }
-         catch (Exception ex)
-         {
-            AutoCAD_PIK_Manager.Log.Error(ex, $"CheckDublicateBlocks - {db.Filename}. {ex.StackTrace}");
-            return;
-         }
-
-         if (AllDublicBlRefInfos.Count==0)
-         {
-            Application.DocumentManager.MdiActiveDocument.Editor.WriteMessage("\nДубликаты блоков не найдены.");
-         }
-         else
-         {
-            foreach (var dublBlRefInfo in AllDublicBlRefInfos)
+        public static void Check()
+        {
+            curDepth = 0;
+            Database db = HostApplicationServices.WorkingDatabase;
+            Inspector.Clear();
+            attemptedblocks = new HashSet<ObjectId>();
+            AllDublicBlRefInfos = new List<BlockRefDublicateInfo>();
+            dictBlRefInfos = new Dictionary<string, Dictionary<PointTree, List<BlockRefDublicateInfo>>>();
+            try
             {
-               Error err = new Error($"Дублирование блоков '{dublBlRefInfo.Name}' - {dublBlRefInfo.CountDublic} шт. в точке {dublBlRefInfo.Position.ToString()}",
-                  dublBlRefInfo.IdBlRef, dublBlRefInfo.TransformToModel, System.Drawing.SystemIcons.Error);
-               err.Tag = dublBlRefInfo;
-               Inspector.Errors.Add(err);
+                using (var t = db.TransactionManager.StartTransaction())
+                {
+                    GetDublicateBlocks(SymbolUtilityServices.GetBlockModelSpaceId(db), Matrix3d.Identity, 0);
+                    t.Commit();
+                }                
+
+                // дублирующиеся блоки
+                AllDublicBlRefInfos = dictBlRefInfos.SelectMany(s => s.Value.Values).Where(w => w.Count > 1)
+                                        .SelectMany(s => s.GroupBy(g => g).Where(w => w.Skip(1).Any()))
+                                        .Select(s =>
+                                           {
+                                               var bi = s.First();
+                                               bi.CountDublic = s.Count();
+                                               bi.Dublicates = s.Skip(1).ToList();
+                                               return bi;
+                                           }).ToList();
+
+                // Добавление дубликатов в результирующий список
+                //AddTransformedToModelDublic(dublicBlRefInfos);
             }
-         }         
-
-         if (Inspector.HasErrors)
-         {
-            var formDublicates = new FormError(true);
-            formDublicates.Text = "Дублирование блоков";
-            formDublicates.EnableDublicateButtons();
-            if (Application.ShowModalDialog(formDublicates) != System.Windows.Forms.DialogResult.OK)
+            catch (Exception ex)
             {
-               formDublicates.EnableDialog(false);
-               Application.ShowModelessDialog(formDublicates);
-               throw new Exception("Отменено пользователем.");
+                AutoCAD_PIK_Manager.Log.Error(ex, $"CheckDublicateBlocks - {db.Filename}. {ex.StackTrace}");
+                return;
             }
-         }
-      }
 
-      private static void GetDublicateBlocks(ObjectId idBtr, Matrix3d transToModel, double rotate)
-      {         
-         // Проверялся ли уже такое определение блока
-         if (attemptedblocks.Add(idBtr))
-         {
-            // такой блок еще не проверялся. Перебор его объетов
-            List<Tuple<ObjectId, Matrix3d, double>> idsBtrNext = new List<Tuple<ObjectId, Matrix3d, double>>();            
-            using (var btr = idBtr.Open(OpenMode.ForRead) as BlockTableRecord)
+            if (AllDublicBlRefInfos.Count == 0)
             {
-               // Получение всех вхождений блоков               
-               foreach (var idEnt in btr)
-               {
-                  using (var blRef = idEnt.Open(OpenMode.ForRead, false, true) as BlockReference)
-                  {
-                     if (blRef == null || !blRef.Visible) continue;
-                     BlockRefDublicateInfo blRefInfo = new BlockRefDublicateInfo(blRef, transToModel, rotate);                     
+                Application.DocumentManager.MdiActiveDocument.Editor.WriteMessage("\nДубликаты блоков не найдены.");
+            }
+            else
+            {
+                foreach (var dublBlRefInfo in AllDublicBlRefInfos)
+                {
+                    Error err = new Error($"Дублирование блоков '{dublBlRefInfo.Name}' - {dublBlRefInfo.CountDublic} шт. в точке {dublBlRefInfo.Position.ToString()}",
+                       dublBlRefInfo.IdBlRef, dublBlRefInfo.TransformToModel, System.Drawing.SystemIcons.Error);
+                    err.Tag = dublBlRefInfo;
+                    Inspector.Errors.Add(err);
+                }
+            }
 
-                     Dictionary<PointTree, List<BlockRefDublicateInfo>> dictPointsBlInfos;
-                     PointTree ptTree = new PointTree(blRefInfo.Position.X, blRefInfo.Position.Y);
+            if (Inspector.HasErrors)
+            {
+                var formDublicates = new FormError(true);
+                formDublicates.Text = "Дублирование блоков";
+                formDublicates.EnableDublicateButtons();
+                if (Application.ShowModalDialog(formDublicates) != System.Windows.Forms.DialogResult.OK)
+                {
+                    formDublicates.EnableDialog(false);
+                    Application.ShowModelessDialog(formDublicates);
+                    throw new Exception("Отменено пользователем.");
+                }
+            }
+        }
 
-                     if (!dictBlRefInfos.TryGetValue(blRefInfo.Name, out dictPointsBlInfos))
-                     {
+        private static void GetDublicateBlocks(ObjectId idBtr, Matrix3d transToModel, double rotate)
+        {
+            // Проверялся ли уже такое определение блока
+            if (attemptedblocks.Add(idBtr))
+            {
+                // такой блок еще не проверялся. Перебор его объетов
+                List<Tuple<ObjectId, Matrix3d, double>> idsBtrNext = new List<Tuple<ObjectId, Matrix3d, double>>();
+                var btr = idBtr.GetObject(OpenMode.ForRead) as BlockTableRecord;
+
+                // Получение всех вхождений блоков               
+                foreach (var idEnt in btr)
+                {
+                    var blRef = idEnt.GetObject(OpenMode.ForRead, false, true) as BlockReference;
+                    if (blRef == null || !blRef.Visible) continue;
+                    BlockRefDublicateInfo blRefInfo = new BlockRefDublicateInfo(blRef, transToModel, rotate);
+
+                    Dictionary<PointTree, List<BlockRefDublicateInfo>> dictPointsBlInfos;
+                    PointTree ptTree = new PointTree(blRefInfo.Position.X, blRefInfo.Position.Y);
+
+                    if (!dictBlRefInfos.TryGetValue(blRefInfo.Name, out dictPointsBlInfos))
+                    {
                         dictPointsBlInfos = new Dictionary<PointTree, List<BlockRefDublicateInfo>>();
                         dictBlRefInfos.Add(blRefInfo.Name, dictPointsBlInfos);
-                        
-                     }
-                     List<BlockRefDublicateInfo> listBiAtPoint;
-                     if (!dictPointsBlInfos.TryGetValue(ptTree, out listBiAtPoint))
-                     {
+                    }
+                    List<BlockRefDublicateInfo> listBiAtPoint;
+                    if (!dictPointsBlInfos.TryGetValue(ptTree, out listBiAtPoint))
+                    {
                         listBiAtPoint = new List<BlockRefDublicateInfo>();
                         dictPointsBlInfos.Add(ptTree, listBiAtPoint);
-                     }
-                     listBiAtPoint.Add(blRefInfo);                     
+                    }
+                    listBiAtPoint.Add(blRefInfo);
 
-                     idsBtrNext.Add(new Tuple<ObjectId, Matrix3d, double>(item1: blRef.BlockTableRecord, item2: blRef.BlockTransform, item3: blRef.Rotation+rotate));
-                  }
-               }
-            }           
+                    idsBtrNext.Add(new Tuple<ObjectId, Matrix3d, double>(item1: blRef.BlockTableRecord, item2: blRef.BlockTransform, item3: blRef.Rotation + rotate));
+                }
 
-            // Нырок глубже
-            if (curDepth < DEPTH)
-            {
-               curDepth++;
-               foreach (var btrNext in idsBtrNext)
-               {
-                  GetDublicateBlocks(btrNext.Item1, btrNext.Item2, btrNext.Item3);
-               }               
+                // Нырок глубже
+                if (curDepth < DEPTH)
+                {
+                    curDepth++;
+                    foreach (var btrNext in idsBtrNext)
+                    {
+                        GetDublicateBlocks(btrNext.Item1, btrNext.Item2, btrNext.Item3);
+                    }
+                }
             }
-         }
-      }
+        }
 
-      //private static void AddTransformedToModelDublic(List<BlockRefDublicateInfo> dublicBlRefInfos)
-      //{
-      //   // Трансформированные копии инфоблоков и добавление в результирующий список дубликатов
-      //   var trancDublicBlRefInfos = dublicBlRefInfos.Select(b => b.TransCopy()).ToList();
-      //   AllDublicBlRefInfos.AddRange(trancDublicBlRefInfos);
-      //}
+        //private static void AddTransformedToModelDublic(List<BlockRefDublicateInfo> dublicBlRefInfos)
+        //{
+        //   // Трансформированные копии инфоблоков и добавление в результирующий список дубликатов
+        //   var trancDublicBlRefInfos = dublicBlRefInfos.Select(b => b.TransCopy()).ToList();
+        //   AllDublicBlRefInfos.AddRange(trancDublicBlRefInfos);
+        //}
 
-      public static void DeleteDublicates(List<Error> errors)
-      {
-         if (errors == null || errors.Count == 0)
-         {
-            return;
-         }
-
-         var blDublicatesToDel = errors.Where(e=>e.Tag!=null && e.Tag is BlockRefDublicateInfo).SelectMany(e => ((BlockRefDublicateInfo)e.Tag).Dublicates);
-         var doc = Application.DocumentManager.MdiActiveDocument;
-         using (doc.LockDocument())
-         {
-            using (var t = blDublicatesToDel.FirstOrDefault()?.IdBlRef.Database.TransactionManager.StartTransaction())
+        public static void DeleteDublicates(List<Error> errors)
+        {
+            if (errors == null || errors.Count == 0)
             {
-               foreach (var dublBl in blDublicatesToDel)
-               {
-                  var blTodel = dublBl.IdBlRef.GetObject(OpenMode.ForWrite, false, true) as BlockReference;
-                  blTodel.Erase();
-               }
-               t.Commit();
+                return;
             }
-         }
-         errors.ForEach(e => Inspector.Errors.Remove(e));
-      }
-   }   
+
+            var blDublicatesToDel = errors.Where(e => e.Tag != null && e.Tag is BlockRefDublicateInfo).SelectMany(e => ((BlockRefDublicateInfo)e.Tag).Dublicates);
+            var doc = Application.DocumentManager.MdiActiveDocument;
+            using (doc.LockDocument())
+            {
+                using (var t = blDublicatesToDel.FirstOrDefault()?.IdBlRef.Database.TransactionManager.StartTransaction())
+                {
+                    foreach (var dublBl in blDublicatesToDel)
+                    {
+                        var blTodel = dublBl.IdBlRef.GetObject(OpenMode.ForWrite, false, true) as BlockReference;
+                        blTodel.Erase();
+                    }
+                    t.Commit();
+                }
+            }
+            errors.ForEach(e => Inspector.Errors.Remove(e));
+        }
+    }
 }
--- AcadLib/Properties/AssemblyInfo.cs
+++ AcadLib/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.64.154")]
-[assembly: AssemblyFileVersion("1.0.64.154")]
+[assembly: AssemblyVersion("1.0.74.155")]
+[assembly: AssemblyFileVersion("1.0.74.155")]
