7a18a0ab70643cddc089d8612c6877fb5f1126df
Vildar Khisyametdinov <vildar82@gmail.com>
2016-03-15 15:20:04 +0300

    BlockList

--- AcadLib/Commands.cs
+++ AcadLib/Commands.cs
@@ -13,53 +13,68 @@ using Autodesk.AutoCAD.Runtime;
 
 namespace AcadLib
 {
-   public class Commands
-   {
-      [CommandMethod("PIK", "PIK-Acadlib-About", CommandFlags.Modal)]
-      public void About ()
-      {
-         Document doc = Application.DocumentManager.MdiActiveDocument;
-         if (doc == null)
-         {
-            return;
-         }
-         Editor ed = doc.Editor;
-         var acadLibVer = Assembly.GetExecutingAssembly().GetName().Version;
-         ed.WriteMessage($"\nБиблиотека AcadLib версии {acadLibVer}");
-      }
-
-      [CommandMethod("PIK", "DbObjectsCountInfo", CommandFlags.Modal)]
-      public void DbObjectsCountInfo()
-      {
-         Document doc = Application.DocumentManager.MdiActiveDocument;
-         if (doc == null) return;
-         Database db = doc.Database;
-         Editor ed = doc.Editor;
-
-         Dictionary<string, int> allTypes = new Dictionary<string, int>();
-
-         for (long i = db.BlockTableId.Handle.Value; i < db.Handseed.Value; i++)
-         {
-            ObjectId id;
-            if (db.TryGetObjectId(new Handle(i), out id))
+    public class Commands
+    {
+        [CommandMethod("PIK", "PIK-Acadlib-About", CommandFlags.Modal)]
+        public void About()
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            if (doc == null)
             {
-               if (allTypes.ContainsKey(id.ObjectClass.Name))
-               {
-                  allTypes[id.ObjectClass.Name]++;
-               }
-               else
-               {
-                  allTypes.Add(id.ObjectClass.Name, 1);
-               }
+                return;
             }
-         }
+            Editor ed = doc.Editor;
+            var acadLibVer = Assembly.GetExecutingAssembly().GetName().Version;
+            ed.WriteMessage($"\nБиблиотека AcadLib версии {acadLibVer}");
+        }
 
-         var sortedByCount = allTypes.OrderBy(i => i.Value);
+        [CommandMethod("PIK", "DbObjectsCountInfo", CommandFlags.Modal)]
+        public void DbObjectsCountInfo()
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            if (doc == null) return;
+            Database db = doc.Database;
+            Editor ed = doc.Editor;
 
-         foreach (var item in sortedByCount)
-         {
-            ed.WriteMessage($"\n{item.Key} - {item.Value}");
-         }
-      }
-   }
+            Dictionary<string, int> allTypes = new Dictionary<string, int>();
+
+            for (long i = db.BlockTableId.Handle.Value; i < db.Handseed.Value; i++)
+            {
+                ObjectId id;
+                if (db.TryGetObjectId(new Handle(i), out id))
+                {
+                    if (allTypes.ContainsKey(id.ObjectClass.Name))
+                    {
+                        allTypes[id.ObjectClass.Name]++;
+                    }
+                    else
+                    {
+                        allTypes.Add(id.ObjectClass.Name, 1);
+                    }
+                }
+            }
+
+            var sortedByCount = allTypes.OrderBy(i => i.Value);
+
+            foreach (var item in sortedByCount)
+            {
+                ed.WriteMessage($"\n{item.Key} - {item.Value}");
+            }
+        }
+
+        [CommandMethod("PIK", "PIK-BlockList", CommandFlags.Modal)]
+        public void BlockListCommand()
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            if (doc == null) return;
+            try
+            {
+                BlockList.List(doc.Database);
+            }
+            catch (System.Exception ex)
+            {
+                doc.Editor.WriteMessage($"\nОшибка - {ex.Message}");
+            }            
+        }
+    }
 }
---
+++ AcadLib/Model/Blocks/BlockList.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.AutoCAD.DatabaseServices;
+using Microsoft.Office.Interop.Excel;
+
+namespace AcadLib
+{
+    public static class BlockList
+    {
+        public static void List(this Database db)
+        {
+            List<string> list = new List<string>();
+            using (var t = db.TransactionManager.StartTransaction())
+            {
+                var bt = db.BlockTableId.GetObject(OpenMode.ForRead) as BlockTable;
+                foreach (var item in bt)
+                {
+                    var btr = item.GetObject(OpenMode.ForRead) as BlockTableRecord;
+                    if (btr != null && !btr.IsLayout && !btr.IsAnonymous && !btr.IsDependent)
+                    {
+                        list.Add(btr.Name);
+                    }
+                }
+                t.Commit();
+            }
+            // Запись в Excel
+            if (list.Count > 0)
+            {
+                var excelApp = new Microsoft.Office.Interop.Excel.Application { DisplayAlerts = false };
+                if (excelApp == null) return;
+
+                // Открываем книгу
+                Workbook workBook = excelApp.Workbooks.Add();
+                Worksheet sheet = workBook.ActiveSheet as Worksheet;
+                sheet.Name = "Блоки";
+                sheet.Cells[1, 1].Value = $"{db.Filename}, {DateTime.Now}";
+
+                sheet.Cells[2, 1].Value = "№пп";
+                sheet.Cells[2, 2].Value = "Имя";
+
+                int row = 3;
+                int count = 1;
+                foreach (var name in list)
+                {
+                    sheet.Cells[row, 1].Value = count.ToString();
+                    count++;
+                    sheet.Cells[row, 2].Value = name;
+                    row++;
+                }
+                excelApp.Visible = true;
+            }            
+        }
+    }
+}
