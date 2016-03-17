8fddb2281cd6ffe2b2125808573531eb6ccedf99
Vildar Khisyametdinov <vildar82@gmail.com>
2016-03-17 10:52:19 +0300

    General

---
+++ AcadLib/General.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcadLib
+{
+    public static class General
+    {
+        /// <summary>
+        /// Отменено пользователем.
+        /// Сообщение для исключения при отмене команды пользователем.
+        /// </summary>
+        public const string CanceledByUser = "Отменено пользователем";
+    }
+}
