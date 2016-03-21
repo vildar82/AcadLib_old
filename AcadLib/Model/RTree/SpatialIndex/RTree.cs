721ae90fd242601fbc47d3810a90e27c081f5c39
Vildar Khisyametdinov <vildar82@gmail.com>
2016-03-21 20:50:02 +0300

    Acadlib.Rtree.SpatialIndex

--- AcadLib/Model/RTree/SpatialIndex/RTree.cs
+++ AcadLib/Model/RTree/SpatialIndex/RTree.cs
@@ -27,7 +27,7 @@ using System;
 using System.Collections;
 
 
-namespace RTreeSpatialIndex
+namespace Acadlib.Rtree.SpatialIndex
 {
 
     /// <summary>
--- AcadLib/Model/RTree/SpatialIndex/Rectangle.cs
+++ AcadLib/Model/RTree/SpatialIndex/Rectangle.cs
@@ -21,7 +21,7 @@
 using System;
 using System.Text;
 
-namespace RTreeSpatialIndex
+namespace Acadlib.Rtree.SpatialIndex
 {
 
     /**
