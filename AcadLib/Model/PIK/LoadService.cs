7c5b18eb1272456547f13a7df71e7859839ee77e
Vildar Khisyametdinov <vildar82@gmail.com>
2016-03-22 10:31:00 +0300

    LoadNetTopologySuite

--- AcadLib/Model/PIK/LoadService.cs
+++ AcadLib/Model/PIK/LoadService.cs
@@ -3,47 +3,56 @@ using System.IO;
 using System.Reflection;
 namespace AcadLib
 {
-   /// <summary>
-   /// Загрузка вспомогательных сборок
-   /// </summary>
-   public static class LoadService
-   {
-      /// <summary>
-      /// Загрузка сборки SpecBlocks.dll - для создания спецификация блоков, в соответствии с настройками.
-      /// </summary>
-      public static void LoadSpecBlocks()
-      {  
-         LoadPackages("SpecBlocks.dll");         
-      }
+    /// <summary>
+    /// Загрузка вспомогательных сборок
+    /// </summary>
+    public static class LoadService
+    {
+        /// <summary>
+        /// Загрузка сборки SpecBlocks.dll - для создания спецификация блоков, в соответствии с настройками.
+        /// </summary>
+        public static void LoadSpecBlocks()
+        {
+            LoadPackages("SpecBlocks.dll");
+        }
 
-      /// <summary>
-      /// Morelinq
-      /// </summary>
-      public static void LoadMorelinq()
-      {         
-         LoadPackages("MoreLinq.dll");
-      }
+        /// <summary>
+        /// Morelinq
+        /// </summary>
+        public static void LoadMorelinq()
+        {
+            LoadPackages("MoreLinq.dll");
+        }
 
-      /// <summary>
-      /// EntityFramework
-      /// </summary>
-      public static void LoadEntityFramework()
-      {  
-         LoadPackages("EntityFramework.dll");         
-         LoadPackages("EntityFramework.SqlServer.dll");
-      }
+        /// <summary>
+        /// EntityFramework
+        /// </summary>
+        public static void LoadEntityFramework()
+        {
+            LoadPackages("EntityFramework.dll");
+            LoadPackages("EntityFramework.SqlServer.dll");
+        }
 
-      private static void LoadPackages(string name)
-      {
-         var dll = Path.Combine(AutoCAD_PIK_Manager.Settings.PikSettings.LocalSettingsFolder, @"Script\NET\packages\" + name);
-         if (File.Exists(dll))
-         {
-            Assembly.LoadFrom(dll);
-         }
-         else
-         {
-            throw new Exception($"Не найден файл {dll}.");
-         }
-      }
-   }
+        /// <summary>
+        /// NetTopologySuite
+        /// </summary>
+        public static void LoadNetTopologySuite()
+        {
+            LoadPackages("NetTopologySuite.dll");
+            LoadPackages("GeoAPI.dll");
+        }
+
+        private static void LoadPackages(string name)
+        {
+            var dll = Path.Combine(AutoCAD_PIK_Manager.Settings.PikSettings.LocalSettingsFolder, @"Script\NET\packages\" + name);
+            if (File.Exists(dll))
+            {
+                Assembly.LoadFrom(dll);
+            }
+            else
+            {
+                throw new Exception($"Не найден файл {dll}.");
+            }
+        }
+    }
 }
