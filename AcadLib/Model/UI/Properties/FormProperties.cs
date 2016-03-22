02070a845cd60a3cbe1bc5bd61604963357e489c
Vildar Khisyametdinov <vildar82@gmail.com>
2016-03-22 13:33:54 +0300

    FormProperties

---
+++ AcadLib/Model/UI/Properties/FormProperties.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AcadLib.UI
+{
+    public partial class FormProperties : Form
+    {
+        public FormProperties()
+        {
+            InitializeComponent();                        
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            propertyGrid1.ResetSelectedProperty();
+        }
+    }
+}
