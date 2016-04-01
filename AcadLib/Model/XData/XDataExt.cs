de056f5bef4a28d0e7047bc2d861541a5c41ffa0
Vildar Khisyametdinov <vildar82@gmail.com>
2016-04-01 13:39:02 +0300

    XDataExt

---
+++ AcadLib/Model/XData/XDataExt.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.AutoCAD.DatabaseServices;
+
+namespace AcadLib
+{
+    public static class XDataExt
+    {
+        public static void RegApp(this Database db, string regAppName)
+        {
+            using (RegAppTable rat = db.RegAppTableId.Open(OpenMode.ForRead, false) as RegAppTable)
+            {
+                if (!rat.Has(regAppName))
+                {
+                    rat.UpgradeOpen();
+                    using (RegAppTableRecord ratr = new RegAppTableRecord())
+                    {
+                        ratr.Name = regAppName;
+                        rat.Add(ratr);
+                    }
+                }
+            }
+        }
+
+        public static void RemoveXData(this DBObject dbo, string regAppName)
+        {
+            if (dbo.GetXDataForApplication(regAppName) != null)
+            {
+                ResultBuffer rb = rb = new ResultBuffer(new TypedValue(1001, regAppName));
+                dbo.UpgradeOpen();
+                dbo.XData = rb;
+                dbo.DowngradeOpen();
+            }
+        }
+
+        public static void SetXData(this DBObject dbo, string regAppName, int value)
+        {
+            using (var rb = new ResultBuffer(
+                        new TypedValue((int)DxfCode.ExtendedDataRegAppName, regAppName),
+                        new TypedValue((int)DxfCode.ExtendedDataInteger32, value)))
+            {
+                dbo.XData = rb;
+            }
+        }
+
+        public static int GetXData(this DBObject dbo, string regAppName)
+        {
+            var rb = dbo.GetXDataForApplication(regAppName);
+            if (rb != null)
+            {
+                foreach (var item in rb)
+                {
+                    if (item.TypeCode == (short)DxfCode.Int32)
+                    {
+                        return (int)item.Value;
+                    }
+                }
+            }
+            return 0;
+        }
+    }
+}
--- AcadLib/Properties/AssemblyInfo.cs
+++ AcadLib/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.92.214")]
-[assembly: AssemblyFileVersion("1.0.92.214")]
+[assembly: AssemblyVersion("1.0.92.223")]
+[assembly: AssemblyFileVersion("1.0.92.223")]
