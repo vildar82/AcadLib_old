08e600e1539160b551604f031daf0cc82c87af79
Vildar Khisyametdinov <vildar82@gmail.com>
2016-04-17 17:51:01 +0300

    RectangleJig

---
+++ AcadLib/Model/Jigs/RectangleJig.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+
+namespace AcadLib.Jigs
+{
+    public class RectangleJig : EntityJig
+    {
+        public Point3d Position { get; set; }
+
+        public RectangleJig(double length, double height) : base(new Polyline())
+        {
+            var pl = (Polyline)Entity;
+            var pt1 = Position.Convert2d();
+            pl.AddVertexAt(0, pt1, 0, 0, 0);
+            pl.AddVertexAt(1, new Point2d (pt1.X,pt1.Y+height), 0, 0, 0);
+            pl.AddVertexAt(2, new Point2d(pt1.X+length, pt1.Y + height), 0, 0, 0);
+            pl.AddVertexAt(3, new Point2d(pt1.X + length, pt1.Y), 0, 0, 0);
+            pl.Closed = true;
+        }
+
+        protected override SamplerStatus Sampler(JigPrompts prompts)
+        {
+            var res = prompts.AcquirePoint("\nТочка вставки разверток:");
+            if (res.Status != PromptStatus.OK)            
+                throw new Exception(General.CanceledByUser);
+
+            SamplerStatus status = SamplerStatus.NoChange;
+            if (!Position.IsEqualTo(res.Value, Tolerance.Global))
+            {
+                status = SamplerStatus.OK;
+            }
+            Position = res.Value; //TransformBy(ed.CurrentUserCoordinateSystem);            
+            return status;
+        }
+
+        protected override bool Update()
+        {
+            try
+            {
+                var pl = (Polyline)Entity;
+                var plPt1 = pl.GetPoint2dAt(0).Convert3d();
+                var vec = Position - plPt1;
+                Matrix3d trans = Matrix3d.Displacement(vec);
+                pl.TransformBy(trans);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }            
+        }
+    }
+}
