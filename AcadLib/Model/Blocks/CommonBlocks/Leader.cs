cc7d6ba6c44c8d9cfe2438c6b703a622ce308036
Vildar Khisyametdinov <vildar82@gmail.com>
2016-08-26 08:50:17 +0300

    leader

--- AcadLib/Model/Blocks/CommonBlocks/Leader.cs
+++ AcadLib/Model/Blocks/CommonBlocks/Leader.cs
@@ -10,8 +10,17 @@ namespace AcadLib.Blocks.CommonBlocks
     public class Leader : BlockBase
     {
         public const string BlockName = "Обозначение_Выноска_ПИК";
+        public const string ParamName = "ОБОЗНАЧЕНИЕ";
+
+        public string Name { get; set; }
         public Leader (BlockReference blRef, string blName) : base(blRef, blName)
         {
+            Name = GetPropValue<string>(ParamName);
+        }
+
+        public void SetName (string value)
+        {
+            FillPropValue(ParamName, value);
         }
     }
 }
