ab00866e7b5024ee2f89dbe03df20b33a732ecf9
Vildar Khisyametdinov <vildar82@gmail.com>
2016-09-19 10:49:57 +0300

    ErrorException

---
+++ AcadLib/Model/Exceptions/ErrorException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.AutoCAD.DatabaseServices;
+
+namespace AcadLib.Exceptions
+{
+    public class ErrorException : Exception
+    {
+        public Errors.Error Error { get; private set; }
+
+        public ErrorException (string msg, Entity ent, Icon icon)
+        {
+            Error = new Errors.Error(msg, ent, icon);
+        }
+
+        public ErrorException(string msg, Icon icon)
+        {
+            Error = new Errors.Error(msg, icon);
+        }
+    }
+}
