7449de908f26ae7167253415ca6c41129f196a92
Vildar Khisyametdinov <vildar82@gmail.com>
2016-10-22 13:36:32 +0300

    Интерфейсы для ExtData

---
+++ AcadLib/Model/XData/Interface/IExtDataSave.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.AutoCAD.ApplicationServices;
+
+namespace AcadLib.XData
+{
+    public interface IExtDataSave
+    {
+        /// <summary>
+        /// Словарь для сохранения объекта
+        /// </summary>        
+        DicED GetExtDic (Document doc);
+        /// <summary>
+        /// установить значения из словаря в объект
+        /// </summary>        
+        void SetExtDic (DicED dicEd, Document doc);
+    }
+}
---
+++ AcadLib/Model/XData/Interface/ITypedDataValues.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+
+namespace AcadLib.XData
+{
+    public interface ITypedDataValues
+    {
+        /// <summary>
+        /// Список сохраняемяхъ значений
+        /// </summary>
+        /// <returns></returns>
+        List<TypedValue> GetDataValues (Document doc);
+        /// <summary>
+        /// Установка значений
+        /// </summary>
+        /// <param name="values"></param>
+        void SetDataValues (List<TypedValue> values, Document doc);
+    }
+}
