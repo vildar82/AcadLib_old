8d865f4ad2ac3406e0761e3060e65da32d7f9431
Vildar Khisyametdinov <vildar82@gmail.com>
2016-12-27 15:36:51 +0300

    UserBreakException

---
+++ AcadLib/Model/Exceptions/UserBreakException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcadLib
+{
+    /// <summary>
+    /// Прерывание долгого процесса пользователем.
+    /// Используется в случае екстремального прерывания - выбрасывается исключение из палитровых методов.
+    /// </summary>
+    public class UserBreakException : Exception
+    {
+    }
+}
