befe452cda1659e2bc925174b814e51eb036f490
Vildar Khisyametdinov <vildar82@gmail.com>
2017-01-10 17:50:56 +0300

    wpf error - dialog

--- AcadLib/Model/WPF/Converters/BooleanToHidingVisibilityConverter.cs
+++ AcadLib/Model/WPF/Converters/BooleanToHidingVisibilityConverter.cs
@@ -15,12 +15,27 @@ namespace AcadLib.WPF.Converters
         public override object Convert (object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (!(value is bool))
-                return Visibility.Hidden;            
+                return Visibility.Collapsed;            
             if ((bool)value)
             {
                 return Visibility.Visible;
             }
-            return Visibility.Hidden;
+            return Visibility.Collapsed;
+        }
+    }
+
+    [ValueConversion(typeof(bool), typeof(Visibility))]
+    public class BooleanToOppositeVisibilityConverter : ConvertorBase
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is bool))
+                return Visibility.Hidden;
+            if ((bool)value)
+            {
+                return Visibility.Hidden;
+            }
+            return Visibility.Visible;
         }
     }
 }
