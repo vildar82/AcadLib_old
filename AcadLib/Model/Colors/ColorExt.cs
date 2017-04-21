6c2f0ad7d88f3a8864e9bdb96fdbb9d45833cdbf
Vildar <vildar82@gmail.com>
2017-04-21 11:12:55 +0300

    colorExt

---
+++ AcadLib/Model/Colors/ColorExt.cs
@@ -0,0 +1,22 @@
+﻿using Autodesk.AutoCAD.Colors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetLib;
+
+namespace AcadLib.Colors
+{
+    public static class ColorExt
+    {
+        public static string AcadColorToStrig(this Color color)
+        {
+            return color.ColorValue.ColorToString();
+        }
+        public static Color AcadColorFeomString(this string color)
+        {
+            return Color.FromColor(color.StringToColor());
+        }
+    }
+}
