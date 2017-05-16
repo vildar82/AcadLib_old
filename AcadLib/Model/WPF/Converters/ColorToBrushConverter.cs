57fb95a8a97a26c0ce0574b63c7274a51322989d
Vildar <vildar82@gmail.com>
2017-05-16 19:44:50 +0300

    fix wpf acad color converter

--- AcadLib/Model/WPF/Converters/ColorToBrushConverter.cs
+++ AcadLib/Model/WPF/Converters/ColorToBrushConverter.cs
@@ -12,21 +12,20 @@ namespace AcadLib.WPF.Converters
     {
         public override object Convert (object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (value is System.Drawing.Color)
+            if (value is System.Drawing.Color dc)
             {
-                var dc = (System.Drawing.Color)value;
                 return new SolidColorBrush(Color.FromArgb(dc.A, dc.R, dc.G, dc.B));
             }
 
-            if (value is Color)
+            if (value is Color mc)
             {
-                return new SolidColorBrush((Color)value);
+                return new SolidColorBrush(mc);
             }
 
-            if (value is Autodesk.AutoCAD.Colors.Color)
+            if (value is Autodesk.AutoCAD.Colors.Color ac)
             {
-                var ac = (Autodesk.AutoCAD.Colors.Color)value;
-                return new SolidColorBrush(Color.FromRgb(ac.Red, ac.Green, ac.Blue));
+                var cv = ac.ColorValue;
+                return new SolidColorBrush(Color.FromRgb(cv.R, cv.G, cv.B));
             }
 
             return null;
