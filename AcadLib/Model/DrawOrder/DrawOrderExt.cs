a046863cc98ecd666bb75dd764f6583213dc7365
Vildar <vildar82@gmail.com>
2017-06-23 19:58:58 +0300

    DrawOrderExt

---
+++ AcadLib/Model/DrawOrder/DrawOrderExt.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.AutoCAD.DatabaseServices;
+
+namespace AcadLib
+{
+    public static class DrawOrderExt
+    {
+        public static void DrawOrder(this BlockTableRecord btr,  ObjectId top, ObjectId bot)
+        {
+            var drawOrder = btr.DrawOrderTableId.GetObject(OpenMode.ForWrite) as DrawOrderTable;
+            if (drawOrder == null) return;
+            var idsAbove = new ObjectIdCollection {top};
+            drawOrder.MoveAbove(idsAbove,bot);
+        }
+    }
+}
