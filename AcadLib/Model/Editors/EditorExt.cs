4b4928d181a2d44ab2e1cec988b96ff2b58cde05
Vildar <vildar82@gmail.com>
2017-08-09 19:35:10 +0300

    Выбор объектов по границе. SelectInExtents

---
+++ AcadLib/Model/Editors/EditorExt.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+
+namespace AcadLib.Editors
+{
+	public static class EditorExt
+	{
+		/// <summary>
+		/// Выбор объектов в заданных границах
+		/// </summary>
+		public static List<ObjectId> SelectInExtents(this Editor ed, Extents3d ext)
+		{
+			ed.Zoom(ext);
+			var selRes = ed.SelectCrossingWindow(ext.MinPoint, ext.MaxPoint);
+			if (selRes.Status == PromptStatus.OK)
+			{
+				return selRes.Value.GetObjectIds().ToList();
+			}
+			throw new Exception($"Ошибка выбора элементов в заданных границах - {ext}");
+		}
+	}
+}
