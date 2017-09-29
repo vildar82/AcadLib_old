a54637d11fa787ce0f8c32fd869b97babcfc43c9
Vildar <vildar82@gmail.com>
2017-09-29 11:50:27 +0300

    AcadHelper - Doc

---
+++ AcadLib/AcadHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.AutoCAD.ApplicationServices;
+
+namespace AcadLib
+{
+    public static class AcadHelper
+    {
+        /// <summary>
+        /// Текущий документ
+        /// </summary>
+        public static Document Doc => Application.DocumentManager.MdiActiveDocument;
+    }
+}
