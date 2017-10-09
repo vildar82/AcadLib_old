7a21dd9461e5a502f031704eb578da365a8d2a40
Vildar <vildar82@gmail.com>
2017-10-09 12:45:42 +0300

    AcadColorToString2

--- AcadLib/Model/Colors/ColorExt.cs
+++ AcadLib/Model/Colors/ColorExt.cs
@@ -15,6 +15,9 @@ namespace AcadLib.Colors
             return Color.FromColor(color.StringToColor());
         }
 
+        /// <summary>
+        /// Определение цвета объекта - если ПоСлою, то возвращает цвет слоя.
+        /// </summary>
         public static Color GetEntityColorAbs(this Entity ent)
         {
             var color = ent.Color;
@@ -24,6 +27,38 @@ namespace AcadLib.Colors
                 color = layer.Color;
             }
             return color;
+        }
+
+        /// <summary>
+        /// Цвет в строку - индекс цвета, или r,g,b.
+        /// </summary>
+        public static string AcadColorToString2(this Color color)
+        {
+            if (color == null) return "";
+            if (color.IsByLayer) return "256";
+            if (color.IsByBlock) return "0";
+            if (color.IsByAci) return color.ColorIndex.ToString();
+            return $"{color.Red},{color.Green},{color.Blue}";
+        }
+
+        /// <summary>
+        /// строку в цвет - из color?.ToString();
+        /// </summary>
+        public static Color AcadColorFromString2(this string color)
+        {
+            if (color.IsNullOrEmpty()) return new Color();
+            // Index
+            if ( short.TryParse(color, out short colorIndex))
+            {
+                return Color.FromColorIndex(ColorMethod.ByAci, colorIndex);
+            }
+            // RGB
+            var rgb = color.Split(',');
+            if (rgb.Length == 3)
+            {
+                return Color.FromRgb(byte.Parse(rgb[0]), byte.Parse(rgb[1]), byte.Parse(rgb[2]));
+            }
+            return new Color();
         }
     }
 }
