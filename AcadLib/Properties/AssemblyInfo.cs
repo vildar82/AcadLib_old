62f7dbeb557a97797cf8f29307f5d3200d6057f8
Vildar <vildar82@gmail.com>
2018-02-26 11:04:43 +0300

    r

--- AcadLib/Properties/AssemblyInfo.cs
+++ AcadLib/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.57.712")]
-[assembly: AssemblyFileVersion("1.0.57.712")]
+[assembly: AssemblyVersion("1.0.57.713")]
+[assembly: AssemblyFileVersion("1.0.57.713")]
