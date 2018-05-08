42a8a11b89f0b5be9e338b59b62827079574cad2
Vildar <vildar82@gmail.com>
2018-05-08 20:22:53 +0300

    автозагрузка лисп файлов из настроек

---
+++ AcadLib/Model/Lisp/LispAutoloader.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using AutoCAD_PIK_Manager.Settings;
+using Autodesk.AutoCAD.ApplicationServices;
+using JetBrains.Annotations;
+using Application = Autodesk.AutoCAD.ApplicationServices.Core.Application;
+using Path = AcadLib.IO.Path;
+
+namespace AcadLib.Lisp
+{
+    public static class LispAutoloader
+    {
+        public static void Start()
+        {
+            Application.DocumentManager.DocumentActivated += DocumentManager_DocumentActivated;
+            foreach (Document doc in Application.DocumentManager)
+            {
+                LoadLisp(doc);
+            }
+        }
+
+        private static void DocumentManager_DocumentActivated(object sender, [NotNull] DocumentCollectionEventArgs e)
+        {
+            LoadLisp(e.Document);
+        }
+
+        private static void LoadLisp(Document doc)
+        {
+            foreach (var refLisp in PikSettings.PikFileSettings.AutoLoadLispPathBySettings)
+            {
+                var startupLispFile = Path.GetLocalSettingsFile(refLisp);
+                if (File.Exists(startupLispFile))
+                {
+                    var lspPath = startupLispFile.Replace('\\', '/');
+                    doc.SendStringToExecute($"(load \"{lspPath}\") ", true, false, true);
+                }
+            }
+        }
+    }
+}
