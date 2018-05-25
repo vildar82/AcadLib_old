c4342578cfd588e0e70a733fec5c7c510ce003fb
Vildar <vildar82@gmail.com>
2018-05-25 15:34:57 +0300

    Обновил подключение к базе статистики.

--- AcadLib/Properties/AssemblyInfo.cs
+++ AcadLib/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.144.870")]
-[assembly: AssemblyFileVersion("1.0.144.870")]
-[assembly: AssemblyInformationalVersion("1.0.97")]
+[assembly: AssemblyVersion("1.0.145.871")]
+[assembly: AssemblyFileVersion("1.0.145.871")]
+[assembly: AssemblyInformationalVersion("1.0.98")]
--- AcadLib/Properties/Settings.Designer.cs
+++ AcadLib/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace AcadLib.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.5.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.7.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -26,8 +26,8 @@ namespace AcadLib.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=cad01pik;Initial Catalog=SAPR;Persist Security Info=True;User ID=cad_" +
-            "ar;Password=qwerty!2345")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=VPP-SQL04;Initial Catalog=CAD_REVIT_STATISTICS;User ID=CAD_AllUsers;P" +
+            "assword=qwerty!2345")]
         public string SAPRConnectionString {
             get {
                 return ((string)(this["SAPRConnectionString"]));
