32c1da1609016d7a2e18d08b679251a34623524a
Vildar <vildar82@gmail.com>
2018-10-14 14:47:12 +0300

    refs path - lib. remove assemblyinfo

--- AcadLib/Properties/AssemblyInfo.cs
+++ AcadLib/Properties/AssemblyInfo.cs
@@ -3,9 +3,6 @@ using System.Runtime.InteropServices;
 using AcadLib;
 using Autodesk.AutoCAD.Runtime;
 
-// Управление общими сведениями о сборке осуществляется с помощью
-// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
-// связанные со сборкой.
 [assembly: AssemblyTitle("AcadLib")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -18,5 +15,3 @@ using Autodesk.AutoCAD.Runtime;
 [assembly: Guid("2008ae5d-550a-4478-a9b5-297832058377")] // Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
 [assembly: CommandClass(typeof(Commands))]
 [assembly: ExtensionApplication(typeof(Commands))]
-[assembly: AssemblyVersion("1.0.287.1133")]
-[assembly: AssemblyFileVersion("1.0.287.1133")]
