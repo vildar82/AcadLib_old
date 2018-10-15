afaacaafe0fa5a3416df825758699558891d855e
Vildar <vildar82@gmail.com>
2018-10-15 12:40:40 +0300

    SummaryInfoExt

---
+++ AcadLib/Model/DB/SummaryInfoExt.cs
@@ -0,0 +1,22 @@
+﻿namespace AcadLib.DB
+{
+    using Autodesk.AutoCAD.DatabaseServices;
+
+    public static class SummaryInfoExt
+    {
+        public static object GetDwgCustomPropertyValue(this Database db, string prop)
+        {
+            var dictProps = db.SummaryInfo.CustomProperties;
+            while (dictProps.MoveNext())
+            {
+                var entry = dictProps.Entry;
+                if (entry.Key == prop)
+                {
+                    return entry.Value;
+                }
+            }
+
+            return null;
+        }
+    }
+}
